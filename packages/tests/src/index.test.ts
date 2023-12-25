89c21d206861ac7963609bc67b322c1a1b48d808
dorofiykolya <dorofiy.kolya@gmail.com>
2023-12-25 01:42:03 +0000

    fix tests

--- packages/tests/src/index.test.ts
+++ packages/tests/src/index.test.ts
@@ -1,10 +1,10 @@
-import { version } from '../../../package.json';
-import { DataIsland } from 'data-island/dist';
+import { version } from '../../../package.json'
+import { app, SDK_VERSION } from 'data-island/dist'
 
 test('SDK_VERSION', () => {
-  expect(DataIsland.SDK_VERSION).toBe(version);
-});
+  expect(SDK_VERSION).toBe(version)
+})
 
 test('API', () => {
-  DataIsland.app();
-});
+  app()
+})
