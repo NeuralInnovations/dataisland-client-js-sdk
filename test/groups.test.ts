ab14177afe802dc148ce0e36b2097b23ccd5ed12
Vladyslav Pavlik <pavliklaw7@gmail.com>
2024-02-09 13:24:22 +0200

    group wip

--- test/groups.test.ts
+++ test/groups.test.ts
@@ -1,6 +1,5 @@
 
 import { testInOrganization } from "./setup"
-
 test("Groups", async () => {
   await testInOrganization(async (app, org) => {
     const groupPromise = org.accessGroups.create("Test group", {isAdmin: true}, ["123"])
@@ -9,12 +8,17 @@ test("Groups", async () => {
 
     const group = await groupPromise
 
+    console.log("group", group)
+
     expect(group).not.toBeUndefined()
 
     expect(group).not.toBeNull()
 
     expect(org.accessGroups.get(group.id)).toBe(group)
 
+    if (!group) {
+      expect(() => group).toThrow("Access group is not loaded.")
+    }
+
   })
 })
-
