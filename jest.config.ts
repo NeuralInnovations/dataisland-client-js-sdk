d130043aeed7af1d966c2d3ccea91c15695bc9a6
dorofiykolya <dorofiy.kolya@gmail.com>
2024-02-25 11:14:03 +0000

    jest timeout, 3min

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,7 @@
 import type { Config } from "jest"
 
 const config: Config = {
-  testTimeout: 60000,
+  testTimeout: 180000,
   // All imported modules in your tests should be mocked automatically
   // automock: false,
 
