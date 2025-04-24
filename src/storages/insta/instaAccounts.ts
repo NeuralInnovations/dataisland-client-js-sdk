f5cee08cc831bbd3b3418c0386b29a0355c9e477
Ihor Zamorskyi <ihorzam25@gmail.com>
2025-04-24 18:41:32 +0300

    release/0.6.12

---
+++ src/storages/insta/instaAccounts.ts
@@ -0,0 +1,30 @@
+import { EventDispatcher } from "../../events"
+import { FlowId } from "./queryFlows"
+import {
+  QueryFileUrlDto,
+  QueryFlowState,
+  QueryFlowStatus
+} from "../../dto/queryFlowResponse"
+
+export enum QueryFlowEvent {
+  UPDATED = "updated"
+}
+
+export abstract class QueryFlow extends EventDispatcher<
+  QueryFlowEvent,
+  QueryFlow
+> {
+
+  abstract get id(): FlowId
+
+  abstract get name(): string
+
+  abstract get state(): QueryFlowState
+
+  abstract get status(): QueryFlowStatus
+
+  abstract get urls(): QueryFileUrlDto[] | undefined
+
+  abstract fetch(): any
+
+}
